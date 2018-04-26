8fa21fc5adfa11962e111e658378734754148759
plansigan <kemchii.69@gmail.com>
2018-04-26 14:09:03 +0800

    several fixes

--- assets/main.js
+++ assets/main.js
@@ -10,13 +10,11 @@ new Vue({
         },
         skills:{
             attack:{
-                name: 'Attack',
-                effect(){
-                    this.healthBars.Monster -= 2
-                }
+                name: 'Attack'
             },
             specialAttack:{
-                name: 'Special Attack'
+                name: 'Special Attack',
+                Used:0
             },
             heal:{
                 name:'Heal'
@@ -24,8 +22,45 @@ new Vue({
             giveUp:{
                 name:'Give Up'
             }
+        }
+    },
+    watch:{
+        Monster(){
+            if(this.healthBars.Monster >= 0){
+                this.healthBars.You -= 5
+            } else {
+                this.Title = 'You Win!'
+            }
         },
+        You(){
+            if (this.healthBars.You <= 0){
+                this.Title = 'You Lose'
+            }
+        }
+    },
+    methods:{
+        wew(skillName) {
+            var specialAttack = this.skills.specialAttack.Used; 
+            if (skillName == 'Attack'){
+                if (this.healthBars.Monster >= 0) { 
+                    this.healthBars.Monster -= 3;
+                }
+            }
+            if (skillName == 'Special Attack' && specialAttack == 0){
+                if (this.healthBars.Monster >= 0) {
+                    this.healthBars.Monster -= 20;
+                    this.skills.specialAttack.Used = 1
+                }
+            }
+        }
     },
     computed:{
+        //for watching Monster nad You variable from data object
+        Monster(){
+            return this.healthBars.Monster;
+        },
+        You() {
+            return this.healthBars.You;
+        }
     }
 })
