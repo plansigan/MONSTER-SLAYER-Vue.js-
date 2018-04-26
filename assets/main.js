3d06f0873f94f38dd70b4789514eeaebdb925a77
plansigan <kemchii.69@gmail.com>
2018-04-26 09:05:20 +0800

    several updates

--- assets/main.js
+++ assets/main.js
@@ -1,6 +1,31 @@
 new Vue({
     el:'#app',
     data:{
-        Title:"Monster Slayer!"
+        Title:"Monster Slayer!",
+        players:['You','Monster'],
+        Game: false,
+        healthBars:{
+            You:100,
+            Monster:100
+        },
+        skills:{
+            attack:{
+                name: 'Attack',
+                effect(){
+                    this.healthBars.Monster -= 2
+                }
+            },
+            specialAttack:{
+                name: 'Special Attack'
+            },
+            heal:{
+                name:'Heal'
+            },
+            giveUp:{
+                name:'Give Up'
+            }
+        },
+    },
+    computed:{
     }
 })
